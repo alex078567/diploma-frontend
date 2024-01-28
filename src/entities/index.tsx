c66e85352bfd6bb06aa6dcc0ca2f1af6fab1bd7b
alex <adb6799@gmail.com>
2024-01-28 15:31:04 +0500

    экспорт сущностей из папки entities

---
+++ src/entities/index.tsx
@@ -0,0 +1,15 @@
+import { LoginForm, RegisterForm } from './forms';
+import HeaderWithText from './header-with-text/HeaderWithText';
+import Loader from './loader/Loader';
+import ProtectedRoute from './protected-route/ProtectedRoute';
+import RedirectedRoute from './redirected-route/RedirectedRoute';
+import TwoButtonsInline from './two-buttons-inline/TwoButtonsInline';
+export {
+	LoginForm,
+	RegisterForm,
+	HeaderWithText,
+	Loader,
+	ProtectedRoute,
+	RedirectedRoute,
+	TwoButtonsInline,
+};
