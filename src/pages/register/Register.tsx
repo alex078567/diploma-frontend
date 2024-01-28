ad60057ec887f98121fa24d92743092a9cf8f728
alex <adb6799@gmail.com>
2024-01-28 16:08:27 +0500

    Компонент "страница регистрации"

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,11 @@
+import { RegisterSection } from 'src/widgets/form-sections';
+
+const Register = () => {
+	return (
+		<main className="form-main register-main">
+			<RegisterSection />
+		</main>
+	);
+};
+
+export default Register;
