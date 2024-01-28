47e73bf516cc857a3b2e18e71f33dd0a51358ab8
alex <adb6799@gmail.com>
2024-01-28 16:06:12 +0500

    Компонент "Домашняя страница" содержащий стили.

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import HomeSection from 'src/widgets/home-section/HomeSection';
+import './Home.scss';
+const Home = () => {
+	return (
+		<main className="home-page-main">
+			<HomeSection />
+		</main>
+	);
+};
+
+export default Home;
