51e6acb4133127a96391af186715abc64916a787
alex <adb6799@gmail.com>
2024-01-28 15:50:03 +0500

    Экспорт компонентов из папки функции (фичи)

---
+++ src/features/index.ts
@@ -0,0 +1,8 @@
+import HeaderLogo from './header-logo/HeaderLogo';
+import {
+	LoginButton,
+	RegisterButton,
+	SignInButton,
+	SignUpButton,
+} from './buttons';
+export { LoginButton, RegisterButton, SignInButton, SignUpButton, HeaderLogo };
