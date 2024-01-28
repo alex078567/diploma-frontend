7134f374a75facdfd6058d7203fc27bccb493d6e
alex <adb6799@gmail.com>
2024-01-28 15:51:41 +0500

    Экспорт кнопок из папки "кнопки"

---
+++ src/features/buttons/index.tsx
@@ -0,0 +1,5 @@
+import LoginButton from './login-button/LoginButton';
+import RegisterButton from './register-button/RegisterButton';
+import SignInButton from './sign-in-button/SignInButton';
+import SignUpButton from './sign-up-button/SignUpButton';
+export { LoginButton, RegisterButton, SignInButton, SignUpButton };
