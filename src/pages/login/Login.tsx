b15c59a4540cbb8f1972cf9cb5f1fe9060e0cc79
alex <adb6799@gmail.com>
2024-01-28 16:08:06 +0500

    Компонент "страница логина"

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import { LoginSection } from 'src/widgets/form-sections/';
+
+const Login = () => {
+	return (
+		<main className="form-main login-main">
+			<LoginSection />
+		</main>
+	);
+};
+
+export default Login;
