0597287e6920d346b36a62e56a267ed869cee560
alex <adb6799@gmail.com>
2024-01-28 16:36:18 +0500

    Компонент "Хедер" содержащий стили и экспорт

---
+++ src/widgets/header/Header.tsx
@@ -0,0 +1,11 @@
+import { HeaderLogo } from 'src/features';
+import './Header.scss';
+const Header = () => {
+	return (
+		<div className="header">
+			<HeaderLogo />
+		</div>
+	);
+};
+
+export default Header;
